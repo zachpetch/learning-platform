c5ddb50efdafd76b5934007ad9a3c5347178bb3b
Zach Petch <zachpetch@gmail.com>
2025-07-18 03:35:38 -0700

    Getting set up for tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get dashboard_url
+    get dashboard_index_url
     assert_response :success
   end
 end
--- test/controllers/schools_controller_test.rb
+++ test/controllers/schools_controller_test.rb
@@ -5,44 +5,8 @@ class SchoolsControllerTest < ActionDispatch::IntegrationTest
     @school = schools(:one)
   end
 
-  test "should get index" do
-    get schools_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_school_url
-    assert_response :success
-  end
-
-  test "should create school" do
-    assert_difference("School.count") do
-      post schools_url, params: { school: { name: @school.name, short_name: @school.short_name } }
-    end
-
-    assert_redirected_to school_url(School.last)
-  end
-
   test "should show school" do
     get school_url(@school)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_school_url(@school)
-    assert_response :success
-  end
-
-  test "should update school" do
-    patch school_url(@school), params: { school: { name: @school.name, short_name: @school.short_name } }
-    assert_redirected_to school_url(@school)
-  end
-
-  test "should destroy school" do
-    assert_difference("School.count", -1) do
-      delete school_url(@school)
-    end
-
-    assert_redirected_to schools_url
   end
 end
